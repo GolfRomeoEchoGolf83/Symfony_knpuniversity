354992346a147a053221334a9e0dfed983455ae4
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-11-18 13:13:32 +0100

    Using service with Controller

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -10,15 +10,21 @@ namespace AppBundle\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class GenusController
+class GenusController extends Controller
 {
 	/**
 	 * @Route("/genus/{genusName")
 	 */
 	public function showAction($genusName)
 	{
+		$templating = $this->container->get('templating');
+		$html = $templating->render('genus/show.html.twig'), array(
+			'name'  => $genusName
+	);
+
 		return new Response('The genus: ' .$genusName);
 	}
 }
